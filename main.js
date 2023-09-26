23e3d16ce2b9888a46eddbe456c776945e2bc590
silviosnjr <silviosnjr@gmail.com>
2023-09-26 10:29:22 -0300

    utilizando as listas para executar uma função.

--- main.js
+++ main.js
@@ -3,4 +3,15 @@ function tocaSomAplausos(){
     document.querySelector("#som_tecla_aplausos").play();
 }
 
-document.querySelectorAll(".tecla");
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+listaDeTeclas[0].onclick = tocaSomAplausos;
+
+/* As linhas abaixo não possuem suas funçõe declaradas */
+listaDeTeclas[2].onclick = tocaSomVaia;
+listaDeTeclas[3].onclick = tocaSomRisada;
+listaDeTeclas[4].onclick = tocaSomGrilo;
+listaDeTeclas[5].onclick = tocaSomTambores;
+listaDeTeclas[6].onclick = tocaSomTrombeta;
+listaDeTeclas[7].onclick = tocaSomDinheiro;
+listaDeTeclas[8].onclick = tocaSomVitoria;
