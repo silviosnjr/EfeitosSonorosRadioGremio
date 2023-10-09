0e7947f5dd3de9622aaaae1eb593bd05abd9f052
Silvio Sales do Nascimento Junior <silviosnjr@gmail.com>
2023-10-09 15:57:04 -0300

    ajustes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function tocaSomRisada(){
-    document.querySelector("#som_tecla_risada").play();
+function tocaSomAplausos(){
+    document.querySelector("#som_tecla_aplausos").play();
 }
 
-document.querySelector(".tecla_risada").onclick = tocaSomRisada;
+document.querySelector(".tecla_aplausos").onclick = tocaSomAplausos;
