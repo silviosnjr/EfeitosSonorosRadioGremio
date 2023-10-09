aad4b9310eeae5c723e69e835c7223566f6a5016
silviosnjr <silviosnjr@gmail.com>
2023-10-09 12:28:28 -0300

    Inserindo funções no evento

--- main.js
+++ main.js
@@ -3,13 +3,18 @@ function tocaSomAplausos(){
     document.querySelector("#som_tecla_aplausos").play();
 }
 
+/* Vaia */
+function tocaSomVaia(){
+    document.querySelector("#som_tecla_vaia").play();
+}
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
 listaDeTeclas[0].onclick = tocaSomAplausos;
+listaDeTeclas[1].onclick = tocaSomVaia;
 
-/* As linhas abaixo não possuem suas funçõe declaradas */
-listaDeTeclas[2].onclick = tocaSomVaia;
-listaDeTeclas[3].onclick = tocaSomRisada;
+/* As linhas abaixo não possuem suas funções declaradas */
+listaDeTeclas[2].onclick = tocaSomRisada;
+listaDeTeclas[3].onclick = tocaSomErrado;
 listaDeTeclas[4].onclick = tocaSomGrilo;
 listaDeTeclas[5].onclick = tocaSomTambores;
 listaDeTeclas[6].onclick = tocaSomTrombeta;
