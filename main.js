4ce6b6cd405debffbbd972dfef9a7aa85bef8b85
silviosnjr <silviosnjr@gmail.com>
2023-10-14 01:00:06 -0300

    contador e while

--- main.js
+++ main.js
@@ -12,11 +12,9 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 listaDeTeclas[0].onclick = tocaSomAplausos;
 listaDeTeclas[1].onclick = tocaSomVaia;
 
-/* As linhas abaixo não possuem suas funções declaradas */
-listaDeTeclas[2].onclick = tocaSomRisada;
-listaDeTeclas[3].onclick = tocaSomErrado;
-listaDeTeclas[4].onclick = tocaSomGrilo;
-listaDeTeclas[5].onclick = tocaSomTambores;
-listaDeTeclas[6].onclick = tocaSomTrombeta;
-listaDeTeclas[7].onclick = tocaSomDinheiro;
-listaDeTeclas[8].onclick = tocaSomVitoria;
+let contador = 0;
+
+//enquanto
+while(){
+    
+}
