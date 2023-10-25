02276e872f91c474ab326aa42c08dd9ebfd1f219
silviosnjr <silviosnjr@gmail.com>
2023-10-24 21:19:37 -0300

    While passou a ser for

--- main.js
+++ main.js
@@ -4,23 +4,13 @@ function tocaSom(idElementoAudio){
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-let contador = 0;
-
-//enquanto
-while(contador < listaDeTeclas.length){
+//para
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
     const tecla = listaDeTeclas[contador];
     const efeito = tecla.classList[1];
-
-    //template string
     const idAudio = `#som_${efeito}`;
-
-    //O template string poderia ser substituído por concatenação
-    //const idAudio = "#som_"+efeito;
 
     tecla.onclick = function (){
         tocaSom(idAudio);
     }
-    contador = contador + 1;
-    
-    //console.log(contador);
 }
