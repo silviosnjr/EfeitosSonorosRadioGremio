4dd7776afe3bc3db01df2bceae034ed7c8104d4f
Silvio Sales do Nascimento Junior <silviosnjr@gmail.com>
2023-10-30 12:33:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ while(contador < listaDeTeclas.length){
     listaDeTeclas[contador].onclick = function (){
         tocaSom(idAudio);
     }
-    contador = contador + 1;
-    
-    //console.log(contador);
+    contador = contador + 1;    
+    console.log(contador);
 }
